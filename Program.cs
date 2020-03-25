e9df2bb3fb606a04ac3e0e516655fdaa1a74266d
soph-russo19 <61275243+soph-russo19@users.noreply.github.com>
2020-03-25 14:29:50 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Git_Diff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The following doscuments are available for comparison: \nGitRepositories_1a \nGitRepositories_2a\nGitRepositories_2b\nGitRepositories_3a\nGitRepositories_3b");
+            Console.WriteLine("Please select the first document you wish to compare: ");
+
+            //User inputs the first file they wish to compare.
+            string First_FilePath = Console.ReadLine();
+
+            // The returned string is stored as a local variable within Main()
+            string First_File = ReadFiles.readFiles(First_FilePath);
+            Console.WriteLine("Please select the second document you wish to compare: ");
+
+            // The same thing is then repeated for the second file
+            string Second_FilePath = Console.ReadLine();
+            string Second_File = ReadFiles.readFiles(Second_FilePath);
+
+            // The bool from comparefiles method is returned and stored locally;
+            bool Files_Same Compare_Files(First_File, Second_File);
+
+            // If the bool is true, then the files are the same 
+            if (Files_Same == true)
+            {
+                Console.WriteLine(" The two files that you have sleected are the same");
+            }
+
+            //Otherwise they are different
+            else
+            {
+                Console.WriteLine("The compared that you have selected are not the same");
+            }
+        }
+
+        // Method for comparing the two text files as arguments
+        static bool Compare_Files(string First_File, string Second_File)
+        {
+            bool Files_Same;
+
+            // if the arguments are the same, return outcome as true
+            if(First_File == Second_File)
+            {
+                Files_Same = true;
+            }
+
+            // If not then return false
+            else
+            {
+                Files_Same = false;
+            }
+            return Files_Same;
+        }
+    }
+
+    // This class reads a singular file
+    class ReadFiles
+    {
+        // The method of the class
+        public static string readFiles(string File_Choice)
+        {
+            // Intialize a StreamReader object
+            StreamReader Text_Object = new StreamReader(@"textFiles/GitRepositories_1a.txt");
+
+            //Creates a file path for StreamReader
+            string File_Input = $@"textFiles/{File_Choice}.txt";
+
+            //Ensures the while loop runs until a correct file path is found
+            bool File_Found = false;
+            while (File_Found == false)
+            {
+                try
+                {
+                    Text_Object = new StreamReader(File_Input);
+
+                    File_Found = true;
+                }
+
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine(" Invalid file selected, pleade try again:");
+
+                    File_Choice = Console.ReadLine();
+                    File_Input = $@"textFiles/{File_Choice}.txt";
+                }
+            }
+
+            string textfile = Text_Object.ReadToEnd();
+
+            return textfile;
+        }
+    }
+}
+
